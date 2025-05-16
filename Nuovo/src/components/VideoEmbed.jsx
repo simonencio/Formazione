63db2c6e6a83e887b8bb937539dab1fb59bc494e
Simone <nencionisimone25@gmail.com>
2025-05-16 10:31:28 +0200

    first commit

---
+++ Nuovo/src/components/VideoEmbed.jsx
@@ -0,0 +1,36 @@
+import ReactPlayer from "react-player";
+
+const VideoEmbed = ({ src, index }) => {
+    if (!ReactPlayer.canPlay(src)) {
+        return null;
+    }
+
+    return (
+        <div key={`video-${index}`} className="w-full flex justify-center lg:justify-start pt-10">
+            <div className="inline-block w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%] 2xl:w-[40%] aspect-[16/9] relative">
+                <ReactPlayer
+                    url={src}
+                    className="absolute top-0 left-0"
+                    width="100%"
+                    height="100%"
+                    controls
+                    config={{
+                        youtube: {
+                            playerVars: {
+                                origin: window.location.origin,
+                                modestbranding: 1,
+                            },
+                        },
+                        vimeo: {
+                            playerOptions: {
+                                responsive: true,
+                            },
+                        },
+                    }}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default VideoEmbed;
---
+++ Nuovo/src/utils/tagMap.js
@@ -0,0 +1,9 @@
+export const tagMap = {
+    p: "mb-4 text-xl leading-relaxed",
+    h1: "text-4xl font-bold py-5 text-[#c22e35] capitalize",
+    h2: "text-3xl font-bold mb-4 capitalize ",
+    h3: "text-2xl font-medium mb-3 mt-5 py-5 capitalize",
+    h4: "text-xl font-medium mb-2 capitalize",
+    h5: "text-lg font-semibold mb-1 capitalize",
+    h6: "text-md font-semibold mb-1 capitalize",
+};
